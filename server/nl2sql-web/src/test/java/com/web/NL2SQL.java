3a7603068770545bd4724c56dd42da9aa05ea9cd
zpei-github <103318397+zpei-github@users.noreply.github.com>
2024-12-03 17:29:10 +0800

    framework construct

---
+++ server/nl2sql-web/src/test/java/com/web/NL2SQL.java
@@ -0,0 +1,11 @@
+package com.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NL2SQL {
+	public static void main(String[] args) {
+		SpringApplication.run(NL2SQL.class, args);
+	}
+}
