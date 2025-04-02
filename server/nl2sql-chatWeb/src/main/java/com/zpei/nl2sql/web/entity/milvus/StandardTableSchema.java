b30e4f1666d43183adbd6a16b946a65673656a7c
zpei-github <103318397+zpei-github@users.noreply.github.com>
2025-04-02 18:59:08 +0800

    结构微调

--- server/nl2sql-chatWeb/src/main/java/com/zpei/nl2sql/web/entity/milvus/StandardTableSchema.java
+++ server/nl2sql-chatWeb/src/main/java/com/zpei/nl2sql/web/entity/milvus/StandardTableSchema.java
@@ -3,11 +3,13 @@ package com.zpei.nl2sql.web.entity.milvus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class StandardTableSchema {
+    @Id
     private Integer standard_table_id;
 
     private String standard_table_name;
